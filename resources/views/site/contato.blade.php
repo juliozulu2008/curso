3f9ae85ccebfdcc6545155472ae5faaacfb5c85a
Julio Cesar <juliozulu2008@gmail.com>
2023-11-08 09:44:02 -0300

    master

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -2,7 +2,18 @@
 @section('title', 'Contato')
 @section('content')
 
-<h1 class="center">Pagina Contato</h1>
+    <div class="col s12 m6 l4 center-align">
+        <img src="{{ asset('images/teams/julio.png') }}" alt="[imagem]" title="jcdsoft" class="responsive-img circle">
+        <p class="flow-text">Julio Cesar Dias Dos Santos</p>
+        <p>Analista Desenvolvedor FullStack</p>
+        <p>
+            <a href="https://www.facebook.com/julio.cesar.diasdosantos/"><i
+                    class="small purple-text fa-brands fa-facebook"></i></a>
+            <a href="https://api.whatsapp.com/send?phone=44998887125"><i
+                    class="small purple-text fa-brands fa-whatsapp"></i></a>
+            <a href="https://www.instagram.com/julio.c.dias/"><i class="small purple-text fa-brands fa-instagram"></i></a>
+        </p>
+    </div>
 
 
 
